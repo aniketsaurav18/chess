761c1444b108cbd3baab3aa310b4fedff181bc17
Saurav Sharma <aniketsaurav18@gmail.com>
2024-09-16 12:25:22 +0000

    added depth slider

--- frontend/src/components/EngineConfiguration.tsx
+++ frontend/src/components/EngineConfiguration.tsx
@@ -1,30 +1,50 @@
 import { Select, SelectItem } from "@nextui-org/select";
 import { Tab, Tabs } from "@nextui-org/tabs";
+import { Slider } from "@nextui-org/slider";
 import { useState } from "react";
 import { EngineDetails } from "../utils/config";
 
+
 const EngineConfiguration = ({ selectedEngine, setSelectedEngine }: any) => {
     return (
-        <Select
-            label="Select Engine"
-            className="max-w-[20rem] w-full m-4"
-            classNames={{
-                listbox: "bg-[#3C3B39] text-white",
-                popoverContent: "bg-[#3C3B39]",
-                // mainWrapper: "bg-[#3C3B39]",
-                // base: "bg-[#3C3B39]",[#3C3B39]
-                trigger: "bg-[#3C3B39]",
-                // innerWrapper: "bg-[#3C3B39]",
-            }}
-            selectedKeys={[selectedEngine]}
-            onSelectionChange={(e) => {
-                e.anchorKey && setSelectedEngine(e.anchorKey);
-            }}
-        >
-            {EngineDetails.map((detail) => (
-                <SelectItem key={detail.key}>{detail.label}</SelectItem>
-            ))}
-        </Select>
+        <div className="flex flex-col justify-center items-center">
+            <Select
+                label="Select Engine"
+                className="max-w-[20rem] w-full m-4"
+                classNames={{
+                    listbox: "bg-[#3C3B39] text-white",
+                    popoverContent: "bg-[#3C3B39]",
+                    // mainWrapper: "bg-[#3C3B39]",
+                    // base: "bg-[#3C3B39]",[#3C3B39]
+                    trigger: "bg-[#3C3B39]",
+                    // innerWrapper: "bg-[#3C3B39]",
+                }}
+                selectedKeys={[selectedEngine]}
+                onSelectionChange={(e) => {
+                    e.anchorKey && setSelectedEngine(e.anchorKey);
+                }}
+            >
+                {EngineDetails.map((detail) => (
+                    <SelectItem key={detail.key}>{detail.label}</SelectItem>
+                ))}
+            </Select>
+            <Slider
+                size="md"
+                step={1}
+                color="foreground"
+                label="Depth"
+                showSteps={true}
+                maxValue={30}
+                minValue={12}
+                defaultValue={20}
+                className="w-2/3"
+                classNames={{
+                    // mark:"bg-red-500",
+                    track: "bg-zinc-400",
+                    // step: "bg-blue-500"
+                }}
+            />
+        </div>
     )
 }
 
