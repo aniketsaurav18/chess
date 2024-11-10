e03a279cc1b56da2f26a3ef2d8fec9be69759469
Saurav Sharma <aniketsaurav18@gmail.com>
2024-11-11 01:14:56 +0530

    added consumer basic

--- backend/prisma/migrations/20241107155518_init/migration.sql
+++ backend/prisma/migrations/20241107155518_init/migration.sql
@@ -29,7 +29,6 @@ CREATE TABLE "Game" (
     "status" "GameStatus" NOT NULL,
     "result" "GameResult",
     "timeControl" "TimeControl" NOT NULL,
-    "moves" JSONB NOT NULL DEFAULT '[]',
     "pgn" TEXT,
     "currentFen" TEXT,
     "startAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -38,6 +37,18 @@ CREATE TABLE "Game" (
     "event" TEXT,
 
     CONSTRAINT "Game_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Move" (
+    "id" TEXT NOT NULL,
+    "gameId" TEXT NOT NULL,
+    "move" TEXT NOT NULL,
+    "moveNum" INTEGER NOT NULL,
+    "fenAfter" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Move_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -50,10 +61,16 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 CREATE INDEX "User_rating_idx" ON "User"("rating");
 
 -- CreateIndex
-CREATE INDEX "Game_status_result_idx" ON "Game"("status", "result");
+CREATE INDEX "Game_id_result_idx" ON "Game"("id", "result");
+
+-- CreateIndex
+CREATE INDEX "Move_gameId_moveNum_idx" ON "Move"("gameId", "moveNum");
 
 -- AddForeignKey
 ALTER TABLE "Game" ADD CONSTRAINT "Game_whitePlayerId_fkey" FOREIGN KEY ("whitePlayerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Game" ADD CONSTRAINT "Game_blackPlayerId_fkey" FOREIGN KEY ("blackPlayerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Move" ADD CONSTRAINT "Move_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "Game"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
