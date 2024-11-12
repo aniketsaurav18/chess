30fb022d76c7747c0c509a607b12adaee9b2e70c
Saurav Sharma <aniketsaurav18@gmail.com>
2024-11-12 22:51:51 +0530

    added function to consumer

---
+++ workers/consumer/processor.ts
@@ -0,0 +1,54 @@
+import { GameOverPayload, MovePayload } from "./types/types";
+
+// {
+//     t: "init_game",
+//     d: {
+//       gameId: this.gameId,
+//       startTime: this.startTime,
+//       timeControl: this.gameTimeLimit,
+//       currentFen:
+//         "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+//     },
+//   }
+
+type InitGamePayload = {
+  t: string;
+  d: {
+    gameId: string;
+    startTime: Date;
+    timeControl: number;
+    currentFen: string;
+  };
+};
+
+const insertMove = async (data) => {
+  console.log(data);
+};
+
+const insertResult = async (data) => {
+  console.log(data);
+};
+
+const insertInitGamePayload = async (data) => {
+  console.log(data);
+};
+
+export const Processor = async (message: string) => {
+  const data = JSON.parse(message) as
+    | MovePayload
+    | GameOverPayload
+    | InitGamePayload;
+  switch (data.t) {
+    case "move": {
+      await insertMove(data.d);
+    }
+
+    case "game_over": {
+      await insertResult(data.d);
+    }
+
+    case "init_game": {
+      await insertInitGamePayload(data.d);
+    }
+  }
+};
